eb2686adbc364fd939402f6051b175869e559f79
zor40 <zor40@mail.ru>
2026-06-14 23:06:48 +0500

    commit

--- src/main/java/ru/hogwarts/school/controller/StudentController.java
+++ src/main/java/ru/hogwarts/school/controller/StudentController.java
@@ -5,6 +5,7 @@ import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.model.Student;
 import ru.hogwarts.school.repository.StudentRepository;
 import ru.hogwarts.school.service.StudentService;
+import ru.hogwarts.school.service.StudentPrintService;
 
 import java.util.Collection;
 import java.util.List;
@@ -15,10 +16,15 @@ public class StudentController {
 
     private final StudentService studentService;
     private final StudentRepository studentRepository;
+    private final StudentPrintService studentPrintService;
 
-    public StudentController(StudentService studentService, StudentRepository studentRepository) {
+    public StudentController(
+            StudentService studentService,
+            StudentRepository studentRepository,
+            StudentPrintService studentPrintService) {
         this.studentService = studentService;
         this.studentRepository = studentRepository;
+        this.studentPrintService = studentPrintService;
     }
 
     @PostMapping
@@ -84,53 +90,11 @@ public class StudentController {
 
     @GetMapping("/print-parallel")
     public void printParallel() {
-        List<Student> students = studentRepository.findAll();
-        if (students.size() < 6) {
-            System.out.println("Not enough students");
-            return;
-        }
-
-        System.out.println(students.get(0).getName());
-        System.out.println(students.get(1).getName());
-
-        Thread thread1 = new Thread(() -> {
-            System.out.println(students.get(2).getName());
-            System.out.println(students.get(3).getName());
-        });
-        thread1.start();
-
-        Thread thread2 = new Thread(() -> {
-            System.out.println(students.get(4).getName());
-            System.out.println(students.get(5).getName());
-        });
-        thread2.start();
-    }
-
-    private synchronized void printName(String name) {
-        System.out.println(name);
+        studentPrintService.printParallel();
     }
 
     @GetMapping("/print-synchronized")
     public void printSynchronized() {
-        List<Student> students = studentRepository.findAll();
-        if (students.size() < 6) {
-            System.out.println("Not enough students");
-            return;
-        }
-
-        printName(students.get(0).getName());
-        printName(students.get(1).getName());
-
-        Thread thread1 = new Thread(() -> {
-            printName(students.get(2).getName());
-            printName(students.get(3).getName());
-        });
-        thread1.start();
-
-        Thread thread2 = new Thread(() -> {
-            printName(students.get(4).getName());
-            printName(students.get(5).getName());
-        });
-        thread2.start();
+        studentPrintService.printSynchronized();
     }
 }
